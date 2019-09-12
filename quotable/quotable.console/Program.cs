43ea5e568263849232c314ce12dbc094d94934c5
matthewod101 <38329959+matthewod101@users.noreply.github.com>
2019-09-12 17:28:15 -0400

    9/12 HW1

---
+++ quotable/quotable.console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using quotable.core;
+
+namespace quotable.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleRandomQuoteProvider s = new SimpleRandomQuoteProvider();
+            long N = long.Parse(Console.ReadLine());
+            foreach(string i in s.FindQuotes(N))
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
---
+++ quotable/quotable.core/SimpleRandomQuoteProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quotable.core
+{
+    public class SimpleRandomQuoteProvider : RandomQuoteProvider
+    {
+        public IEnumerable<string> FindQuotes(long val)
+        {
+
+            string[] test = new string[3];
+            test[0] = "Bad boys for life";
+            test[1] = "Live and let die";
+            test[2] = "Go hard or go home";
+            string[] home;
+            if (val <= 3) {
+                home = new string[val];
+            }
+            else { home = new string[3]; }
+            for (int i = 0; i < home.Length; i++)
+            {
+                Random r = new Random();
+                home[i] = test[r.Next(0,3)];
+            }
+            return home;
+        }
+    }
+}
